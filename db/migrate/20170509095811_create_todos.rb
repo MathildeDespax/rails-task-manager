bcdad990c04abde5200e732455859f5b42ae93aa
Mathilde Despax <mat.despax@gmail.com>
2017-05-09 17:13:45 +0200

    Tasklist

--- db/migrate/20170509095811_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[5.0]
-  def change
-    create_table :todos do |t|
-      t.string :item
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509095811) do
+ActiveRecord::Schema.define(version: 20170509101317) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "item"
